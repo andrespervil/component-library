1710e2cd566ce865682eab609e7c24656ee9b085
Andrés Pérez <a.perez@shakersworks.com>
2022-11-11 19:50:07 +0100

    project setup (jest, ts, storybook, rollup...)

---
+++ .storybook/main.js
@@ -0,0 +1,47 @@
+const path = require('path')
+
+module.exports = {
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  framework: '@storybook/react',
+  stories: ['../src/**/*.stories.tsx'],
+  addons: ['@storybook/addon-controls', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            importLoaders: 1,
+          },
+        },
+        {
+          loader: 'postcss-loader',
+          options: {
+            sourceMap: true,
+            postcssOptions: {
+              plugins: [['postcss-preset-env']],
+            },
+          },
+        },
+        'sass-loader',
+      ],
+      include: path.resolve(__dirname, '../'),
+    })
+
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [['react-app', { flow: false, typescript: true }]],
+      },
+    })
+    config.resolve.extensions.push('.ts', '.tsx')
+
+    return config
+  },
+}
